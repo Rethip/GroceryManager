0811d5a7699be88243506aa146d2425792363046
Rethip <46738995+Rethip@users.noreply.github.com>
2019-03-20 11:40:14 -0700

    assignment

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { GroceryComponent } from './app-grocery';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GroceryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [GroceryComponent]
+})
+export class AppModule { }
